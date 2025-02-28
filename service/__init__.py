9d40c5e1c5a7cc81ba90d61e2188295ad0781595
Surinder Ram <surinder.ram@gmail.com>
2025-02-28 04:17:24 -0500

    Added security header

--- service/__init__.py
+++ service/__init__.py
@@ -7,6 +7,8 @@ and SQL database
 import sys
 
 from flask import Flask
+from flask_talisman import Talisman
+
 
 from service import config
 from service.common import log_handlers
@@ -14,6 +16,8 @@ from service.common import log_handlers
 # Create Flask application
 app = Flask(__name__)
 app.config.from_object(config)
+talisman = Talisman(app)
+
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
