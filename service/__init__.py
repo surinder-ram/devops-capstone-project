3c9d66c8bde45ac796a01dd6f3e0ba338f996016
Surinder Ram <surinder.ram@gmail.com>
2025-02-28 04:27:30 -0500

    Added security headerand cors2

--- service/__init__.py
+++ service/__init__.py
@@ -8,7 +8,7 @@ import sys
 
 from flask import Flask
 from flask_talisman import Talisman
-
+from flask_cors import CORS
 
 from service import config
 from service.common import log_handlers
@@ -17,7 +17,7 @@ from service.common import log_handlers
 app = Flask(__name__)
 app.config.from_object(config)
 talisman = Talisman(app)
-
+CORS(app)
 
 # Import the routes After the Flask app is created
 # pylint: disable=wrong-import-position, cyclic-import, wrong-import-order
